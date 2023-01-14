45cfc4fd5a2c8f3acef7d563b20da93db95ea2b0
Valentin Cabrin <valentin.cabrin@etu.uca.fr>
2023-01-14 09:51:32 +0100

    Dice Controller
    Creation of a dice controller

---
+++ Sources/ApiREST/Controllers/ValuesController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StubLib;
+using EntitiesLib;
+using BuisnessLib;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiREST.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+
+        private readonly ModelManager _service;
+
+        public DiceController(ModelManager service)
+        {
+            _service = service;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IEnumerable<Dice>> Get(int id)
+        {
+            try
+            {
+                var result = await _service.GetAllDices();
+
+                if( result == null)
+                {
+                    return NotFound();
+                }
+
+            } catch(Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from the database");
+            }
+        }
+
+        
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Dice>> Create(Dice dice) // Prendre pas un DIce mais un DTO
+        {
+            try
+            {
+                var createDice = await _service.AddDice(dice);
+                if( !createDice)
+                {
+                    return BadRequest();
+                }
+
+                // CreatedAtAction va retourne l'objet créé, et sur lequelle on va ensuite le convertire avec le DTO pour ensuite le return
+                return CreatedAtAction(nameof(GetAllDices),
+                    new { id = createDice.id }, createDice);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from the database");
+            }
+        }
+        
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
+
