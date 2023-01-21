700fba61ebed1e01f3d84fb46caf260d4a8bc6f5
Augustin GIRAUDIER <augiraudie>
2023-01-21 08:37:02 +0100

    classes DTO et methodes d'extention tomodel/todto

--- Sources/DTO/DTOExtentions.cs
+++ Sources/DTO/DTOExtentions.cs
@@ -5,14 +5,62 @@ namespace DTO
     public static class DTOExtentions
     {
 
+        // ----------- TO MODEL ----------- //
         public static Dice ToModel(this DiceDTO dto)
         {
-            // TODO : ajouter les classes dto pour gérer les sides et na pas laisser null
-            var ds = new Dice(new SecureRandomizer(), null);
+            DiceSideType[] dsts = new DiceSideType[dto.SideTypes.Count()];
+            int cpt = 0;
+            foreach(DiceSideTypeDTO dstdto in dto.SideTypes)
+            {
+                dsts[cpt] = dto.SideTypes[cpt].ToModel();
+                cpt++;
+            }
+            var ds = new Dice(new SecureRandomizer(), dsts);
+            ds.Id = dto.ID;
+            return ds;
+        }
+        public static DiceSideType ToModel(this DiceSideTypeDTO dto)
+        {
+            var dst = new DiceSideType(dto.nbPrototype, dto.prototype.ToModel());
+            return dst;
+        }
+        public static DiceSide ToModel(this DiceSideDTO dto)
+        {
+            var ds = new DiceSide(dto.image);
             ds.Id = dto.ID;
             return ds;
         }
 
-        // ...
+
+        // ----------- TO DTO ----------- //
+        public static DiceDTO ToDTO(this Dice model)
+        {
+            DiceSideTypeDTO[] dsts = new DiceSideTypeDTO[model.SideTypes.Count()];
+            int cpt = 0;
+            foreach (DiceSideType dstdto in model.SideTypes)
+            {
+                dsts[cpt] = model.SideTypes[cpt].ToDTO();
+                cpt++;
+            }
+            var dto = new DiceDTO();
+            dto.ID = model.Id;
+            dto.SideTypes = dsts;
+            return dto;
+        }
+        public static DiceSideTypeDTO ToDTO(this DiceSideType model)
+        {
+            var dto = new DiceSideTypeDTO();
+            dto.nbPrototype = model.NbSide;
+            dto.prototype = model.Prototype.ToDTO();
+            return dto;
+        }
+        public static DiceSideDTO ToDTO(this DiceSide model)
+        {
+            var dto = new DiceSideDTO();
+            dto.image = model.Image;
+            dto.ID = model.Id;
+            return dto;
+        }
+
     }
 }
--- Sources/DTO/DiceDTO.cs
+++ Sources/DTO/DiceDTO.cs
@@ -2,8 +2,8 @@
 {
     public class DiceDTO
     {
-        public int ID { get; set; }
+        public long ID { get; set; }
 
-        // TODO : ajouter la classe dto pour les faces (ou générer une string exploitable par l'interface web)
+        public DiceSideTypeDTO[] SideTypes { get; set; }
     }
 }
