ad4e4bda6230fc797d3a3e713266058b96197899
Augustin Giraudier <giraudieraugustin@gmail.com>
2023-02-10 20:57:21 +0100

    suppression greeter service et protos

--- Sources/ApiGRPC/Program.cs
+++ Sources/ApiGRPC/Program.cs
@@ -17,7 +17,6 @@ builder.Services.AddScoped<IDataManager, StubedDatabaseLinker>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
 app.MapGrpcService<SideService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
--- Sources/ClientGRPC/Program.cs
+++ Sources/ClientGRPC/Program.cs
@@ -4,18 +4,10 @@ using Grpc.Net.Client;
 // The port number must match the port of the gRPC server.
 using var channel = GrpcChannel.ForAddress("https://localhost:5001");
 
-
-// test greeters
-var client = new Greeter.GreeterClient(channel);
-var reply = await client.SayHelloAsync(
-                  new HelloRequest { Name = "GreeterClient" });
-Console.WriteLine("Greeting: " + reply.Message);
-
 // get sides :
 var client2 = new Sides.SidesClient(channel);
 var reply2 = await client2.getSidesAsync(new SideRequest { Id = 2 });
 Console.WriteLine("Side: " + reply2.Id + " img : " + reply2.Image);
-
 
 
 Console.WriteLine("Press any key to exit...");
