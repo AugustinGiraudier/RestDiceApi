d3e13d1cf2e5658991b42e889a5e948310274646
Augustin Giraudier <giraudieraugustin@gmail.com>
2023-02-10 20:52:48 +0100

    ajout de la recuperation de side dans le client

--- Sources/ApiGRPC/Program.cs
+++ Sources/ApiGRPC/Program.cs
@@ -1,4 +1,8 @@
 using ApiGRPC.Services;
+using Microsoft.EntityFrameworkCore;
+using EntitiesLib;
+using ModelAppLib;
+using StubEntitiesLib;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,11 +11,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddDbContext<DiceLauncherDbContext>(opt => opt.UseInMemoryDatabase("dbDice"));
+builder.Services.AddScoped<IDataManager, StubedDatabaseLinker>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
+app.MapGrpcService<SideService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
