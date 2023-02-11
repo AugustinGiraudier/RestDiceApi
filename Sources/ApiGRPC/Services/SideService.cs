419f98202d90eabab6f2eb0d973f2e9f0cd74656
Augustin Giraudier <giraudieraugustin@gmail.com>
2023-02-11 09:44:19 +0100

    ajout log grpc sides

--- Sources/ApiGRPC/Services/SideService.cs
+++ Sources/ApiGRPC/Services/SideService.cs
@@ -43,24 +43,30 @@ namespace ApiGRPC.Services
 
         public override async Task<SideReply> deleteSide(SideRequest request, ServerCallContext context)
         {
+            _logger.LogTrace($"delete side with id={request.Id}");
             var diceSideToDelete = await _manager.GetDiceSideWithId(request.Id);
 
             var resultDelete = await _manager.DeleteSide(diceSideToDelete);
 
             if (!resultDelete)
             {
-                
+                _logger.LogError($"Unable to find Side with id={request.Id}");
+                throw new RpcException(new Status(StatusCode.NotFound, "Unable to delete Side..."));
             }
+            _logger.LogTrace("delete side success");
             return diceSideToDelete.ToReply();
         }
         public async override Task<SideReply> addSide(InputSideRequest request, ServerCallContext context)
         {
+            _logger.LogTrace("add side");
             var ds = new DiceSide(request.Image);
             var addedDice = await _manager.AddSide(ds);
             if (!addedDice)
             {
+                _logger.LogError("Unable to add new Side");
                 throw new RpcException(new Status(StatusCode.NotFound, "Unable to add Side..."));
             }
+            _logger.LogTrace("add side success");
             return ds.ToReply();
         }
 
